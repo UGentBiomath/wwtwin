8993284da3712cb9570cea1df54dd148e35bca27
sdaneshg <saba.daneshgar@ugent.be>
2025-11-27 09:40:32 +0100

    small restructuring

---
+++ src/wwtwin/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for wwtwin."""
+
+__author__ = """Saba Daneshgar"""
+__email__ = 'sabadaneshgar@gmail.com'
+__version__ = '0.1.0'
+
+
+from .database import DataBase
+from .data_processor import DataProcessor
+
